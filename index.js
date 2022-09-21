037d86e83591e882a34669c63d318fcf03ddf345
Brandon Moreno <brandonm99@me.com>
2022-09-21 17:05:21 -0600

    commit tree 2a9a67fac0162703e94707c9374632672673b983

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let createEle = "tagName"
+
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "Brandon Moreno is the champion";
